2c7cef14238ace5047a0b2d4aa1dd99b54fd06eb
pablo <pnavgarb@g.educaand.es>
2023-03-17 11:03:15 +0100

    hashcode empleado

--- src/main/java/ejercicioHospital/Empleado.java
+++ src/main/java/ejercicioHospital/Empleado.java
@@ -4,6 +4,8 @@
  */
 package ejercicioHospital;
 
+import java.util.Objects;
+
 /**
  *
  * @author pablo
@@ -42,6 +44,29 @@ public abstract class Empleado extends Persona {
         this.salario = salario;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.numeroSeguridadSocial);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        return Objects.equals(this.numeroSeguridadSocial, other.numeroSeguridadSocial);
+    }
+
+    
     @Override
     public String toString() {
         return super.toString()+"\n Empleado{" + "numeroSeguridadSocial=" + numeroSeguridadSocial + ", salario=" + salario + '}';
