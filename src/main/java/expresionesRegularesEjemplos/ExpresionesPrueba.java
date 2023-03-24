43130ac71bd69845a63b06ba2706a3ad577b1f40
pablo <pnavgarb@g.educaand.es>
2023-03-24 11:12:44 +0100

    Expresionesss

---
+++ src/main/java/expresionesRegularesEjemplos/ExpresionesPrueba.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expresionesRegularesEjemplos;
+
+/**
+ *
+ * @author pablo
+ */
+public class ExpresionesPrueba {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //A.Detector N,S,E,O
+        //Introducir un texto y detectar la posicion en la que se 
+        //encuentra la letra,
+        //indicando los puntos cardinales(norte,sur,este y oeste).
+        
+        //B.En cualquier String, buscar si esta contenido,en alguna posicion,
+        // que este la palabra 'ini', '2 numeros' y 'fin'
+        
+        //C.Que cumpla que una letra con numero tantas veces como se quiera
+        //ej:a7b5c9z;
+        
+        
+    }
+    
+    
+    
+}
